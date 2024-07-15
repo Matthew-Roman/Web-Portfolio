9a03a7f29b511a5d461e2af6cdd557df2cb6b843
unknown <romanmatthew85@gmail.com>
2024-07-14 17:21:41 -0700

    first push

---
+++ src/constants.js
@@ -0,0 +1,52 @@
+export const scaleFactor = 4;
+
+export const dialogueData = {
+    welcome: `<b>WELCOME!</b> <br><br> 
+    There are only two rooms, the one above
+    is my pursuits and the one ahead is about me. I would like to extend a huge thanks to <a href="https://www.youtube.com/@JSLegendDev">JSLegendDev</a> 
+    because of a tutorial he made on how to do a site like this.`,
+   
+    "about me": `<b>ABOUT ME</b><br><br>
+    Keeping it brief, I enjoy computer science and over the years have slowly
+    developed a like for math. At the same time I've also started to, hehe, hate web development(hence the game your on). I guess it's the 
+    html, css, and js part that kills me. Anyways, as of right now I'm quite broke and have no job. Aside from my career related things 
+    I enjoy Minecraft pvp and cooking. Aaaaaaaand that's it, I got nothing more to say about myself.`,
+   
+    fridge: `<b>FRIDGE</b> <br><br>
+    This probably one of the worst ideas I have on this site cause of the constant updates, 
+    but I figure why not discuss what I want to eat or have eaten today. Today is 7/14/2024 and the first thing I ate was my moms breakfast, 
+    as always it's super! As of now I haven't eaten dinner or lunch but then again my lazy self got up at 9 today, so ... I'm feeling Nashville 
+    hot chicken today, sounds yummy!`,
+   
+    misc: `<b>MISC</b> <br><br> 
+    Random things I find on the internet, 
+    <ul>
+        <li>
+            <a href="">egg</a>
+        </li>
+    </ul>`,
+    
+    hobbies: `<b>HOBBIES</b> <br><br> 
+    I really like to cook, it's something about making your own food over eating at a place that makes cooking so much fun. 
+    I'm still learning a ton but watching Nick DiGiovanni and master chef always made me want to do better on my cooking skills. My goal 
+    for this hobby is to eventually do a beef wellington, which is not easy by any means but it will be very rewarding. 
+    <br><br>
+    Aside from cooking I'm a hardcore Minecraft pvp'er, it's not a word but who cares! I really hoped to get a gaming pc this year, but my 
+    trashy-self for some reason can't get a job so my dreams of recording some of my cracked gameplay are ruined. Sorry but I have to vent,
+    if I had a pc I would be a way better better player as my Xbox One severly holds me back. Hopefully I get one soon as it's not 
+    only useful for that but also many other things related to my career.`,
+    
+    achievements: `<b>ACHIEVEMENTS</b> <br><br> 
+    Idk, I'm not really impressive at the moment.`,
+    
+    "current work": `<b>CURRENT WORK</b> <br><br> 
+    I'm currently reading a discrete math book from school that basically no one in the class finished because it was so dang
+    time consuming, especially in the middle of school year. I did figure though that reading this book will help me advanced in CS and I could 
+    to a lot of mini projects due to its nature. Aside from that I'm also practicing for the Alec's test because I don't want to do these useless
+    math courses in college.`,
+
+    "past work": `<b>PAST WORK</b> <br><br> 
+    As time progresses I will import all my work from my old site to the new one, at the current moment I feel unmotivated to do a 
+    bunch of work. This procrastination is gonna bite me back me real bad lol.`
+
+}
---
+++ src/main.js
@@ -0,0 +1,210 @@
+import { dialogueData, scaleFactor } from "./constants";
+import { k } from "./kaboomCtx";
+import { displayDialogue, setCamScale } from "./utils";
+
+k.loadSprite("spritesheet", "/spritesheet.png", {
+    sliceX: 39, 
+    sliceY: 31,
+    anims: {
+        // from = start frame, to = end frame, speed = fps
+     "idle-down": 936,
+     "walk-down": { from: 936, to: 939, loop: true, speed: 8 },
+     "idle-side": 975,
+     "walk-side": { from: 975, to: 978, loop: true, speed: 8 },
+     "idle-up": 1014,
+     "walk-up": { from: 1014, to: 1017, loop: true, speed: 8 },
+    },
+});
+
+k.loadSprite("map", "./map.png");
+
+k.setBackground(k.Color.fromHex("#311047"));
+
+//game logic
+k.scene("main", async () => {
+    const mapData = await (await fetch("./map.json")).json();
+    const layers = mapData.layers; // this is how layers are accessed and likewise there objects
+
+    const map = k.add([
+        k.sprite("map"),
+        k.pos(0),
+        k.scale(scaleFactor),
+    ]);
+
+    const player = k.make([
+        k.sprite("spritesheet", { anim: "idle-down" }), //defualt anim
+        k.area({shape: new k.Rect(k.vec2(0,3), 10, 10)}),
+        k.body(),
+        k.anchor("center"),
+        k.pos(),
+        k.scale(scaleFactor), 
+        {
+            speed: 250, 
+            direction: "down",
+            isInDialogue: false,
+        },
+        "player",  
+    ]);
+    //
+    for (const layer of layers) {
+        if (layer.name === "boundries") {
+            for (const boundry of layer.objects) {
+                map.add([ //this make a boundry when collision is found. it does it with the rect function and gets the heihgt and width from th json file
+                    k.area({
+                        shape: new k.Rect(k.vec2(0), boundry.width, boundry.height),
+                    }),
+                    k.body({isStatic: true}),
+                    k.pos(boundry.x, boundry.y),
+                    boundry.name,
+                ]);
+
+                if (boundry.name) {
+                    player.onCollide(boundry.name, () => { //this prevents player from moving while reading text
+                        player.isInDialogue = true; 
+                        displayDialogue(dialogueData[boundry.name], () => (player.isInDialogue = false)); // lets player move again
+                    });
+                }
+            }
+            continue;
+        }
+
+        if (layer.name === "spawnpoint") {
+            for (const entity of layer.objects) {
+                if (entity.name === "spawnpoint") { 
+                    //this is bad naming, but it gets the layer spawnpoint from the json and then looks through the layer itself
+                    //for the spawnpoint object. spawnpoint in in the 2nd if should be called "player" but im to lazy, so hopefully this isnt 
+                    //bad in the future
+                    player.pos = k.vec2(
+                        (map.pos.x + entity.x) * scaleFactor,
+                        (map.pos.y + entity.y) * scaleFactor
+                    );
+                    k.add(player);  
+                    continue;
+                }
+            }
+        }
+    }
+
+    //this helps adjust game with screen sizes
+    setCamScale(k)
+    k.onResize(() => {
+        setCamScale(k);
+    });
+
+    k.onUpdate(() => {
+        k.camPos(player.pos.x, player.pos.y + 100);
+    });
+
+    k.onMouseDown((mouseBtn) => {
+        if (mouseBtn !== "left" || player.isInDialogue) return;
+        const worldMousePos = k.toWorld(k.mousePos());
+        player.moveTo(worldMousePos, player.speed);
+
+        // the below is used for different anims of the player moving
+        //if the mouse/tap is found at a specific angle(think of a circle around player) then thats the anim
+        const mouseAngle = player.pos.angle(worldMousePos)
+
+        const lowerBound = 50;
+        const upperBound = 125;
+
+        if (
+            (mouseAngle > lowerBound) && (mouseAngle < upperBound) && (player.curAnim() !== "walk-up") 
+        ) {
+            player.play("walk-up");
+            player.direction = "up";
+            return;
+        }
+        if (
+            mouseAngle < -lowerBound &&
+            mouseAngle > -upperBound &&
+            player.curAnim() !== "walk-down"
+          ) {
+            player.play("walk-down");
+            player.direction = "down";
+            return;
+          }
+      
+          if (Math.abs(mouseAngle) > upperBound) {
+            player.flipX = false;
+            if (player.curAnim() !== "walk-side") player.play("walk-side");
+            player.direction = "right";
+            return;
+          }
+      
+          if (Math.abs(mouseAngle) < lowerBound) {
+            player.flipX = true;
+            if (player.curAnim() !== "walk-side") player.play("walk-side");
+            player.direction = "left";
+            return;
+          }
+     });
+     // start of stopping anims
+     function stopAnims() {
+        if (player.direction === "down") {
+          player.play("idle-down");
+          return;
+        }
+        if (player.direction === "up") {
+          player.play("idle-up");
+          return;
+        }
+    
+        player.play("idle-side");
+      }
+    
+      k.onMouseRelease(stopAnims);
+    
+      k.onKeyRelease(() => {
+        stopAnims();
+      });
+      k.onKeyDown((key) => {
+        const keyMap = [
+          k.isKeyDown("right"),
+          k.isKeyDown("left"),
+          k.isKeyDown("up"),
+          k.isKeyDown("down"),
+        ];
+    
+        let nbOfKeyPressed = 0;
+        for (const key of keyMap) {
+          if (key) {
+            nbOfKeyPressed++;
+          }
+        }
+    
+        if (nbOfKeyPressed > 1) return;
+    
+        if (player.isInDialogue) return;
+        if (keyMap[0]) {
+          player.flipX = false;
+          if (player.curAnim() !== "walk-side") player.play("walk-side");
+          player.direction = "right";
+          player.move(player.speed, 0);
+          return;
+        }
+    
+        if (keyMap[1]) {
+          player.flipX = true;
+          if (player.curAnim() !== "walk-side") player.play("walk-side");
+          player.direction = "left";
+          player.move(-player.speed, 0);
+          return;
+        }
+    
+        if (keyMap[2]) {
+          if (player.curAnim() !== "walk-up") player.play("walk-up");
+          player.direction = "up";
+          player.move(0, -player.speed);
+          return;
+        }
+    
+        if (keyMap[3]) {
+          if (player.curAnim() !== "walk-down") player.play("walk-down");
+          player.direction = "down";
+          player.move(0, player.speed);
+        }
+      });
+});
+
+k.go("main");
+

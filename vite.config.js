4514d4d6e0da0039a287b3b30a675e14b6c3d0d9
unknown <romanmatthew85@gmail.com>
2024-07-15 16:53:59 -0700

    small comments in code for reference

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,7 @@
 import { defineConfig } from "vite";
 
+// do all changes on dist folder first as it provides the 'npm run preivew' command
+// once it looks good then change the actual files in public, src, etc ... 
 export default defineConfig({
     base: "/Web-Portfolio",
     build: {
